f6db80c235ae38f23b034a4322d97cfe732292fa
Kate Fike <kfike@pop-os.localdomain>
2023-09-20 22:47:38 -0400

    Initial commit

---
+++ vesta_asylum/words.py
@@ -0,0 +1,46 @@
+ADJECTIVES = [
+    "infected",
+    "swollen",
+    "misaligned",
+    "ruptured",
+    "fused",
+    "congenital",
+    "diseased",
+    "missing",
+    "smelly",
+    "severed",
+    "crushed",
+    "dislocated",
+    "mangled",
+    "icky",
+    "unrecognizable",
+    "contorted",
+    "bloated",
+    "botched",
+    "oozing",
+    "deteriorated",
+    "abominable"
+]
+
+BODY_PARTS = [
+    "brain",
+    "eye",
+    "nose",
+    "mouth",
+    "testicle",
+    "buttox",
+    "rectal",
+    "hand",
+    "foot",
+]
+
+PROCEDURES = [
+    "extraction",
+    "removal",
+    "lobotomy",
+    "replacement",
+    "transplant",
+    "examination",
+    "expansion",
+    "whitening",
+]
