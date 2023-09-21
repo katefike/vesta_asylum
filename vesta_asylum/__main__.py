76ae0162edd5d1e5ae57666f9b8f39ec17d94ef6
Kate Fike <kfike@pop-os.localdomain>
2023-09-20 22:48:13 -0400

    Use vestaboard installable

--- vesta_asylum/__main__.py
+++ vesta_asylum/__main__.py
@@ -1,34 +1,25 @@
-import requests
-import json
+import vestaboard
+import sys
 from loguru import logger
+import random
 
 from . import ENV
+from .words import ADJECTIVES, BODY_PARTS, PROCEDURES
 
 logger.add(sink="main.log", level="INFO")
 
-HEADERS = {
-    "Content-Type": "application/json",
-    "X-Vestaboard-Read-Write-Key": ENV["API_KEY"]
-}
-
-def get_message():
-    url = "https://rw.vestaboard.com/"
-
-    response = requests.get(url, headers=HEADERS)
-
-    logger.info(response.text)
-    return response.status_code
-
-def post_message():
-    url = "https://rw.vestaboard.com/"
-    data = {
-        "text": "Hello World !!!"
-    }
-
-    response = requests.post(url, headers=HEADERS, data=json.dumps(data))
-    logger.info(response.text)
-    return response.status_code
+def _get_proc_body_adj():
+    return (
+        random.choice(ADJECTIVES),
+        random.choice(BODY_PARTS),
+        random.choice(PROCEDURES),
+    )
 
 if __name__ == "__main__":
-    get_message()
-    post_message()
+    name = sys.argv[1]
+    installation = vestaboard.Installable(
+        ENV["KEY"], ENV["SECRET"], saveCredentials=False
+    )
+    board = vestaboard.Board(installation)
+    procedure = _get_proc_body_adj()
+    board.post(f"{name}'s\n{procedure[0]}\n{procedure[1]}\n{procedure[2]}")
