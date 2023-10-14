12a53bcc01abb1da084d35b3f7e690f1331d7494
kfike <kate.e.fike@gmail.com>
2023-10-13 22:50:32 -0400

    Change app name from polls to appointments

--- vesta_asylum/settings.py
+++ vesta_asylum/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'appointments.apps.AppointmentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
