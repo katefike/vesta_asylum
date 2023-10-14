d186fdf4e7c9be4268062e6e63d085e54c3cb789
kfike <kate.e.fike@gmail.com>
2023-10-14 17:14:30 -0400

    Give a response if no patient name is entered

--- appointments/views.py
+++ appointments/views.py
@@ -7,7 +7,9 @@ from .forms import InputForm
 def index(request):
     if request.method == 'POST':
         patient_name = request.POST['patient_name']
-        return HttpResponse(f'Hi {patient_name}!')
+        if not patient_name:
+            return HttpResponse('Please enter your name and press submit...and we will make you submit.')
+        return HttpResponse(f'Successfully scheduled an appointment for {patient_name}.')
     context ={}
     context['form']= InputForm()
     return render(request, "appointments/index.html", context)
