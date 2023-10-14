4dbebf794c2736c0ad50ab88becb94702ff042fb
kfike <kate.e.fike@gmail.com>
2023-10-14 18:04:56 -0400

    return appointment

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+'''
+Remember the three-step guide to making model changes:
 
-# Create your models here.
+Change your models (in models.py).
+Run python manage.py makemigrations to create migrations for those changes
+Run python manage.py migrate to apply those changes to the database.
+'''
+import datetime
+
+from django.contrib import admin
+from django.db import models
+from django.utils import timezone
+
+class Appointments(models.Model):
+    question_text = models.CharField(max_length=200)
+    post_date = models.DateTimeField("time posted")
+
+    def __str__(self):
+        return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering="post_date",
+        description="Posted recently?",
+    )
+    def was_posted_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.post_date <= now
--- appointments/views.py
+++ appointments/views.py
@@ -26,17 +26,19 @@ def post_appointment(patient_name):
     date = now.strftime("%m.%d.%Y")
     time = now.strftime("%I:%M%p")
     procedure = _get_proc_body_adj()
-    board.post(f"EMERGENCY PROCEDURES\n{date} {time}\n\nPATIENT: {patient_name}\n{procedure[0]} {procedure[1]}\n{procedure[2]}")
-    return True
+    appointment = f"EMERGENCY PROCEDURES\n{date} {time}\n\nPATIENT: {patient_name}\n{procedure[0]} {procedure[1]}\n{procedure[2]}"
+    board.post(appointment)
+    return appointment
 
 def index(request):
     if request.method == 'POST':
         patient_name = request.POST['patient_name']
         if not patient_name:
             return HttpResponse('Please enter your name and press "Submit"...and we will make you submit.')
-        if not post_appointment(patient_name):
+        appointment = post_appointment(patient_name)
+        if not appointment:
             return HttpResponse(f'FAILED to schedule an appointment for {patient_name}.')
-        return HttpResponse(f'Successfully scheduled an appointment for {patient_name}.')
+        return HttpResponse(f'Successfully scheduled an appointment: {appointment}.')
     context ={}
     context['form']= PatientNameForm()
     return render(request, "appointments/index.html", context)
