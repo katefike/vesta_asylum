d6d94e8074a020ae8ccc94bfdbc2516fb46535cd
kfike <kate.e.fike@gmail.com>
2023-10-14 17:32:54 -0400

    get rid of second button

--- appointments/views.py
+++ appointments/views.py
@@ -8,11 +8,8 @@ def index(request):
     if request.method == 'POST':
         patient_name = request.POST['patient_name']
         if not patient_name:
-            return HttpResponse('Please enter your name and press submit...and we will make you submit.')
-        if 'random' in request.POST:
-            return HttpResponse(f'Successfully scheduled an appointment for {patient_name}.')
-        if 'choosing' in request.POST:
-            return HttpResponse(f'Ok {patient_name}...we\'ll let you choose...this time.')      
+            return HttpResponse('Please enter your name and press "Submit"...and we will make you submit.')
+        return HttpResponse(f'Successfully scheduled an appointment for {patient_name}.') 
     context ={}
     context['form']= PatientNameForm()
     return render(request, "appointments/index.html", context)
