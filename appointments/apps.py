ae1d3d6ac8d73224899c817431424289288f9f33
kfike <kate.e.fike@gmail.com>
2023-10-13 22:50:38 -0400

    Change app name from polls to appointments

--- appointments/apps.py
+++ appointments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
+class AppointmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'madlibs'
+    name = 'appointments'
