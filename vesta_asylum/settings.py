15ad2c81c471f1025615763c2b974160fcf544d4
kfike <kate.e.fike@gmail.com>
2023-10-14 11:02:14 -0400

    Working hello world at /appointments

--- vesta_asylum/settings.py
+++ vesta_asylum/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'appointments.apps.AppointmentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
