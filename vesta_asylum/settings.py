283837b64bf3bf7490d0bb639624b1de78bd4819
kfike <kate.e.fike@gmail.com>
2023-10-15 11:47:13 -0400

    Delete polls app

--- vesta_asylum/settings.py
+++ vesta_asylum/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'appointments.apps.AppointmentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
